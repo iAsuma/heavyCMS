548a9542f9850da68e68f8a298f6e3d042dc0589
li shuaiqiu <lishuaiqiu@qizuang.com>
2022-03-14 14:44:14 +0800

    shop模块的说明

--- app/shop/route/route.php
+++ app/shop/route/route.php
@@ -1,6 +1,12 @@
 <?php
 use think\facade\Route;
 
+/*
+ *
+ * 商城路由未完全实现，若需要使用请自行实现，
+ * 由于启用了强制路由，若没有配置路由将会报错
+ *
+ * */
 Route::group('/', function(){
     Route::group('', function (){
         Route::get('/', 'shop/Index/index');
